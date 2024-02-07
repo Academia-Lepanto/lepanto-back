4d3ad0db415c520ccb15695b105827c9804bb2f1
Daniel Martínez <dani.martinez.3.fernandez@gmail.com>
2024-02-07 18:11:59 +0100

    Revert "Error CORS"
    This reverts commit 35dbe5b6d6a9a9853a71a64dddf32318b244675c.

--- testing-ws/src/main/java/com/lepanto/testing/ws/core/rest/MainRestController.java
+++ testing-ws/src/main/java/com/lepanto/testing/ws/core/rest/MainRestController.java
@@ -1,19 +1,15 @@
 package com.lepanto.testing.ws.core.rest;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {
-    "http://www.psicotecnicos-admin.academialepanto.com"}, allowCredentials = "true")
 @RestController
 public class MainRestController {
-
-  @RequestMapping(value = "/main", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-  public String main() {
-    return "index";
-  }
-
+	@RequestMapping(value = "/main", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String main() {
+		return "index";
+	}
+	
 }
--- testing-ws/src/main/java/com/lepanto/testing/ws/core/rest/QuestionRestController.java
+++ testing-ws/src/main/java/com/lepanto/testing/ws/core/rest/QuestionRestController.java
@@ -3,24 +3,21 @@ package com.lepanto.testing.ws.core.rest;
 import com.lepanto.testing.api.core.service.IQuestionService;
 import com.ontimize.jee.server.rest.ORestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@CrossOrigin(origins = {
-    "http://www.psicotecnicos-admin.academialepanto.com"}, allowCredentials = "true")
 @RestController
 @RequestMapping("/questions")
 public class QuestionRestController extends ORestController<IQuestionService> {
 
-  @Autowired
-  IQuestionService iQuestionService;
+    @Autowired
+    IQuestionService iQuestionService;
 
-  @Override
-  public IQuestionService getService() {
-    return this.iQuestionService;
-  }
+    @Override
+    public IQuestionService getService() {
+        return this.iQuestionService;
+    }
 
 
 }
